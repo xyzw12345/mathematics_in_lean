d12db972035c76962a4c60b62c4ab9b05460a261
xyzw12345 <wangjt2020@163.com>
2024-05-16 17:24:07 +0800

    Update S04_More_on_Order_and_Divisibility.lean

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,38 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm (max_le _ _) (max_le _ _)
+  exact le_max_right b a
+  exact le_max_left b a
+  exact le_max_right a b
+  exact le_max_left a b
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm (le_min _ (le_min _ _)) (le_min (le_min _ _) _)
+  exact le_trans (min_le_left _ _) (min_le_left _ _)
+  exact le_trans (min_le_left _ _) (min_le_right _ _)
+  exact min_le_right _ _
+  exact min_le_left _ _
+  exact le_trans (min_le_right _ _) (min_le_left _ _)
+  exact le_trans (min_le_right _ _) (min_le_right _ _)
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  exact le_min (add_le_add_right (min_le_left a b) c) (add_le_add_right (min_le_right a b) c)
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm (aux a b c)
+  have : min a b = a ∨ min a b = b := by exact min_choice a b
+  rcases this with (hl | hr)
+  rw [hl]; apply min_le_left
+  rw [hr]; apply min_le_right
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  have : _ := abs_add (a - b) b
+  rw [sub_add a b b, sub_self, sub_zero] at this
+  rw [tsub_le_iff_right]; exact this
+
 end
 
 section
@@ -66,7 +87,11 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add (dvd_add _ _) _
+  rw [← mul_assoc, mul_comm y x, mul_assoc]; exact dvd_mul_right x _
+  rw [pow_two]; exact dvd_mul_right x _
+  rw [pow_two]; exact dvd_mul_of_dvd_left h _
+
 end
 
 section
@@ -78,7 +103,11 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply dvd_antisymm (dvd_gcd _ _) (dvd_gcd _ _)
+  exact gcd_dvd_right m n
+  exact gcd_dvd_left m n
+  exact gcd_dvd_right n m
+  exact gcd_dvd_left n m
+
+
 end
-
-
