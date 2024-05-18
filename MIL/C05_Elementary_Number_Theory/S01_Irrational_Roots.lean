a34bbaf38d2a001e144b071a7e5466e8ba203af5
xyzw12345 <wangjt2020@163.com>
2024-05-18 13:04:34 +0800

    Update S01_Irrational_Roots.lean

--- MIL/C05_Elementary_Number_Theory/S01_Irrational_Roots.lean
+++ MIL/C05_Elementary_Number_Theory/S01_Irrational_Roots.lean
@@ -52,23 +52,51 @@ example (a b c : Nat) (h : a * b = a * c) (h' : a ≠ 0) : b = c :=
 example {m n : ℕ} (coprime_mn : m.Coprime n) : m ^ 2 ≠ 2 * n ^ 2 := by
   intro sqr_eq
   have : 2 ∣ m := by
-    sorry
+    have : 2 ∣ (m * m) := by
+      rw [← pow_two m, sqr_eq]; exact dvd_mul_right 2 (n ^ 2)
+    have : _ := Nat.prime_two.dvd_mul.mp this
+    simp only [or_self] at this; exact this
   obtain ⟨k, meq⟩ := dvd_iff_exists_eq_mul_left.mp this
   have : 2 * (2 * k ^ 2) = 2 * n ^ 2 := by
     rw [← sqr_eq, meq]
     ring
-  have : 2 * k ^ 2 = n ^ 2 :=
-    sorry
+  have : 2 * k ^ 2 = n ^ 2 := by
+    exact Nat.mul_left_cancel (by norm_num) this
   have : 2 ∣ n := by
-    sorry
+    have : 2 ∣ (n * n) := by
+      rw [← pow_two n, ← this]; exact dvd_mul_right 2 (k ^ 2)
+    have : _ := Nat.prime_two.dvd_mul.mp this
+    simp only [or_self] at this; exact this
   have : 2 ∣ m.gcd n := by
-    sorry
+    apply dvd_gcd (by assumption) (by assumption)
   have : 2 ∣ 1 := by
-    sorry
+    rw [coprime_mn] at this; exact this
   norm_num at this
 
 example {m n p : ℕ} (coprime_mn : m.Coprime n) (prime_p : p.Prime) : m ^ 2 ≠ p * n ^ 2 := by
-  sorry
+  intro sqr_eq
+  have : p ∣ m := by
+    have : p ∣ (m * m) := by
+      rw [← pow_two m, sqr_eq]; exact dvd_mul_right p (n ^ 2)
+    have : _ := (Nat.Prime.dvd_mul prime_p).mp this
+    simp only [or_self] at this; exact this
+  obtain ⟨k, meq⟩ := dvd_iff_exists_eq_mul_left.mp this
+  have : p * (p * k ^ 2) = p * n ^ 2 := by
+    rw [← sqr_eq, meq]
+    ring
+  have : p * k ^ 2 = n ^ 2 := by
+    exact Nat.mul_left_cancel (by exact Nat.Prime.pos prime_p) this
+  have : p ∣ n := by
+    have : p ∣ (n * n) := by
+      rw [← pow_two n, ← this]; exact dvd_mul_right p (k ^ 2)
+    have : _ := (Nat.Prime.dvd_mul prime_p).mp this
+    simp only [or_self] at this; exact this
+  have : p ∣ m.gcd n := by
+    apply dvd_gcd (by assumption) (by assumption)
+  have : p ∣ 1 := by
+    rw [coprime_mn] at this; exact this
+  simp only [Nat.dvd_one] at this; rw [this] at prime_p; absurd prime_p; norm_num
+
 #check Nat.factors
 #check Nat.prime_of_mem_factors
 #check Nat.prod_factors
@@ -93,9 +121,11 @@ example {m n p : ℕ} (nnz : n ≠ 0) (prime_p : p.Prime) : m ^ 2 ≠ p * n ^ 2 
   intro sqr_eq
   have nsqr_nez : n ^ 2 ≠ 0 := by simpa
   have eq1 : Nat.factorization (m ^ 2) p = 2 * m.factorization p := by
-    sorry
+    rw [Nat.factorization_pow]; simp only [Finsupp.coe_smul, Pi.smul_apply, smul_eq_mul]
   have eq2 : (p * n ^ 2).factorization p = 2 * n.factorization p + 1 := by
-    sorry
+    rw [Nat.factorization_mul, Nat.factorization_pow, Nat.Prime.factorization]
+    simp only [Finsupp.coe_add, Finsupp.coe_smul, nsmul_eq_mul, Nat.cast_ofNat, Pi.add_apply, Finsupp.single_eq_same, Pi.mul_apply, Pi.ofNat_apply]
+    rw[add_comm]; exact prime_p; exact Nat.Prime.ne_zero prime_p; exact nsqr_nez
   have : 2 * m.factorization p % 2 = (2 * n.factorization p + 1) % 2 := by
     rw [← eq1, sqr_eq, eq2]
   rw [add_comm, Nat.add_mul_mod_self_left, Nat.mul_mod_right] at this
@@ -107,14 +137,18 @@ example {m n k r : ℕ} (nnz : n ≠ 0) (pow_eq : m ^ k = r * n ^ k) {p : ℕ} (
   · simp
   have npow_nz : n ^ k ≠ 0 := fun npowz ↦ nnz (pow_eq_zero npowz)
   have eq1 : (m ^ k).factorization p = k * m.factorization p := by
-    sorry
+    rw [Nat.factorization_pow]; simp only [Finsupp.coe_smul, Pi.smul_apply, smul_eq_mul]
   have eq2 : (r.succ * n ^ k).factorization p =
       k * n.factorization p + r.succ.factorization p := by
-    sorry
+    rw [Nat.factorization_mul, Nat.factorization_pow]
+    simp only [Nat.succ_eq_add_one, Finsupp.coe_add, Finsupp.coe_smul, nsmul_eq_mul, Pi.natCast_def,
+      Nat.cast_id, Pi.add_apply, Pi.mul_apply]; rw [add_comm]; simp only [Nat.succ_eq_add_one,
+        ne_eq, add_eq_zero, one_ne_zero, and_false, not_false_eq_true]; simp only [ne_eq,
+          pow_eq_zero_iff', not_and, Decidable.not_not]; simp only [nnz, false_implies]
   have : r.succ.factorization p = k * m.factorization p - k * n.factorization p := by
     rw [← eq1, pow_eq, eq2, add_comm, Nat.add_sub_cancel]
   rw [this]
-  sorry
+  use (m.factorization p - n.factorization p)
+  rw [Nat.mul_sub_left_distrib]
 
 #check multiplicity
-
